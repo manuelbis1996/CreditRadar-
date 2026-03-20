e71c8e4e1263863f06426f5b0c1cbf1bcaab557b
Manuelbis Encarnacion <manuelbis1996@gmail.com>
2026-03-20 00:09:33 -0400

    18.5

--- creditradar.user.js
+++ creditradar.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CreditRadar 📶
 // @namespace    http://tampermonkey.net/
-// @version      18.4
+// @version      18.5
 // @description  Organizador inteligente de disputes - clasifica colecciones, acreedores, inquiries e información personal automáticamente
 // @author       
 // @match        https://pulse.disputeprocess.com/*
@@ -10,7 +10,7 @@
 // @downloadURL  https://raw.githubusercontent.com/manuelbis1996/CreditRadar-/main/creditradar.user.js
 // ==/UserScript==
 
-const SCRIPT_VERSION = "18.4";
+const SCRIPT_VERSION = "18.5";
 
 (function () {
 'use strict';
